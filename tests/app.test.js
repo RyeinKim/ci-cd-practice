ed035a35f1ab8645adb1ffaac6ae46a8ef1da6f1
RyeinKim <ryein6896@gmail.com>
2024-07-30 17:07:46 +0900

    Change response message to Hello, Universe! and update tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,13 +4,13 @@ const express = require('express');
 
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello, Universe!'); // 메시지 변경
 });
 
 describe('GET /', () => {
-    it('should return Hello, World!', async () => {
+    it('should return Hello, Universe!', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);
-        expect(res.text).toEqual('Hello, World!');
+        expect(res.text).toEqual('Hello, Universe!'); // 테스트 메시지 변경
     });
 });
